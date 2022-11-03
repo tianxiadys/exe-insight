c235df873b983fca43fc792abd64119b1685ffbc
gaomingyu <874774259@qq.com>
2022-11-03 15:19:38 +0800

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+import App from './index.svelte'
+import './index.css'
+
+const target = document.getElementById('app')
+new App({ target })
---
+++ index.svelte
@@ -0,0 +1,4 @@
+<button on:click={()=>a1++}>{a1}</button>
+<script>
+  let a1 = 0
+</script>

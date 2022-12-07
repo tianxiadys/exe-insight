f9bda0d3640d57b01ed0f36d4d27665844d42a8d
gaomingyu <874774259@qq.com>
2022-12-07 16:50:19 +0800

    新的代码结构

--- src/format/windows/header_dos.js
+++ src/format/windows/header_dos.js
@@ -1,20 +1,20 @@
 import HeaderNT from './header_nt.js'
 
 export default async function(file) {
-  //初始化
-  let blob = file.slice(0, 64)
-  let buffer = await blob.arrayBuffer()
-  let view = new DataView(buffer)
-  let result = {}
-  //读取结构
-  result.Type = view.getUint16(0, true)
-  result.LfaNew = view.getUint32(60, true)
-  //检查类型
-  if (result.Type === 0x5A4D) {
-    result.NT = await HeaderNT(file, result.LfaNew)
-  } else {
-    throw Error('not a dos file')
-  }
-  //返回
-  return result
+    //初始化
+    let blob = file.slice(0, 64)
+    let buffer = await blob.arrayBuffer()
+    let view = new DataView(buffer)
+    let result = {}
+    //读取结构
+    result.Type = view.getUint16(0, true)
+    result.LfaNew = view.getUint32(60, true)
+    //检查类型
+    if (result.Type === 0x5A4D) {
+        result.NT = await HeaderNT(file, result.LfaNew)
+    } else {
+        throw Error('not a dos file')
+    }
+    //返回
+    return result
 }
--- src/format/windows/header_nt.js
+++ src/format/windows/header_nt.js
@@ -2,20 +2,20 @@ import HeaderCOFF from './header_coff.js'
 import HeaderPE from './header_pe.js'
 
 export default async function(file, offset) {
-  //初始化
-  let blob = file.slice(offset, offset + 4)
-  let buffer = await blob.arrayBuffer()
-  let view = new DataView(buffer)
-  let result = {}
-  //读取结构
-  result.Type = view.getUint32(0, true)
-  //检查类型
-  if (result.Type === 0x4550) {
-    result.COFF = await HeaderCOFF(file, offset + 4)
-    result.PE = await HeaderPE(file, offset + 24, result.COFF.SizeOfOptionalHeader)
-  } else {
-    throw Error('not a pe file')
-  }
-  //返回
-  return result
+    //初始化
+    let blob = file.slice(offset, offset + 4)
+    let buffer = await blob.arrayBuffer()
+    let view = new DataView(buffer)
+    let result = {}
+    //读取结构
+    result.Type = view.getUint32(0, true)
+    //检查类型
+    if (result.Type === 0x4550) {
+        result.COFF = await HeaderCOFF(file, offset + 4)
+        result.PE = await HeaderPE(file, offset + 24, result.COFF.SizeOfOptionalHeader)
+    } else {
+        throw Error('not a pe file')
+    }
+    //返回
+    return result
 }

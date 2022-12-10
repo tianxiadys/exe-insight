3f2b0b8f02e7cf731223cc579464516964155eb9
gaomingyu <874774259@qq.com>
2022-12-10 18:51:05 +0800

    延迟导入表

--- src/format/windows/dictionary_import.js
+++ src/format/windows/dictionary_import.js
@@ -1,62 +1,106 @@
-export default async function(image, dictionary) {
-    //初始化
+export default async function(image, dictionary, delay) {
+    if (delay) {
+        return parseImportDelay(image, dictionary)
+    } else {
+        return parseImportNormal(image, dictionary)
+    }
+}
+
+async function parseImportNormal(image, dictionary) {
     let resultList = []
-    //循环读取
     for (let index1 = 0; ; index1++) {
-        //初始化
         let view = await image.pointerToView(dictionary.VritualAddress + index1 * 20, 20)
         let result = {}
-        //读取结构
         result.Index = index1
         result.OriginalFirstThunk = view.getUint32(0, true)
         result.TimeDateStamp = view.getUint32(4, true)
         result.ForwarderChain = view.getUint32(8, true)
         result.Name = view.getUint32(12, true)
         result.FirstThunk = view.getUint32(16, true)
-        //检查停止位
+        //空元素终止
         if (result.FirstThunk === 0) {
             break
         }
-        //附加字段
+        //实际解释
         result.NameString = await image.pointerToString(result.Name, false)
-        result.RESULT = []
-        //处理内容
-        let thunkView = await image.pointerToView(result.OriginalFirstThunk || result.FirstThunk)
-        if (image.BITS === 32) {
-            for (let index2 = 0; ; index2++) {
-                let item = {}
-                let thunkItem = thunkView.getUint32(index2 * 4, true)
-                if (thunkItem === 0) {
-                    break
-                } else if (thunkItem > 0x80000000) {
-                    item.Index = index2
-                    item.Ordinal = thunkItem & 0xFFFF
-                } else {
-                    item.Index = index2
-                    item.Name = await image.pointerToString(thunkItem + 2, false)
-                }
-                result.RESULT.push(item)
-            }
-        } else if (image.BITS === 64) {
-            for (let index2 = 0; ; index2++) {
-                let item = {}
-                let thunkItem = thunkView.getBigUint64(index2 * 8, true)
-                if (thunkItem === 0n) {
-                    break
-                } else if (thunkItem > 0x8000000000000000n) {
-                    item.Index = index2
-                    item.Ordinal = Number(thunkItem & 0xFFFFn)
-                } else {
-                    let pointer = Number(thunkItem + 2n)
-                    item.Index = index2
-                    item.Name = await image.pointerToString(pointer, false)
-                }
-                result.RESULT.push(item)
-            }
-        }
-        //添加到结果数组
+        result.RESULT = await parseImportItem(image, result)
         resultList.push(result)
     }
-    //返回
     return resultList
+}
+
+async function parseImportDelay(image, dictionary) {
+    let resultList = []
+    for (let index1 = 0; ; index1++) {
+        let view = await image.pointerToView(dictionary.VritualAddress + index1 * 32, 32)
+        let result = {}
+        //字段名称非原始名称
+        result.Index = index1
+        result.Characteristics = view.getUint32(0, true)
+        result.Name = view.getUint32(4, true)
+        result.ModuleHandle = view.getUint32(8, true)
+        result.FirstThunk = view.getUint32(12, true)
+        result.OriginalFirstThunk = view.getUint32(16, true)
+        result.BoundTable = view.getUint32(20, true)
+        result.UnloadTable = view.getUint32(24, true)
+        result.TimeDateStamp = view.getUint32(28, true)
+        //空元素终止
+        if (result.FirstThunk === 0) {
+            break
+        }
+        //实际解释
+        result.NameString = await image.pointerToString(result.Name, false)
+        result.RESULT = await parseImportItem(image, result)
+        resultList.push(result)
+    }
+    return resultList
+}
+
+async function parseImportItem(image, result) {
+    //部分文件的OriginalFirstThunk字段为空
+    let view = await image.pointerToView(result.OriginalFirstThunk || result.FirstThunk)
+    if (image.BITS === 32) {
+        return parseImportItem32(image, view)
+    } else {
+        return parseImportItem64(image, view)
+    }
+}
+
+async function parseImportItem32(image, view) {
+    let itemList = []
+    for (let index2 = 0; ; index2++) {
+        let item = {}
+        let thunkItem = view.getUint32(index2 * 4, true)
+        if (thunkItem === 0) {
+            break
+        } else if (thunkItem > 0x80000000) {
+            item.Index = index2
+            item.Ordinal = thunkItem & 0xFFFF
+        } else {
+            item.Index = index2
+            item.Name = await image.pointerToString(thunkItem + 2, false)
+        }
+        itemList.push(item)
+    }
+    return itemList
+}
+
+async function parseImportItem64(image, view) {
+    let itemList = []
+    for (let index2 = 0; ; index2++) {
+        let item = {}
+        let thunkItem = view.getBigUint64(index2 * 8, true)
+        if (thunkItem === 0n) {
+            break
+        } else if (thunkItem > 0x8000000000000000n) {
+            item.Index = index2
+            item.Ordinal = Number(thunkItem & 0xFFFFn)
+        } else {
+            let pointer = Number(thunkItem + 2n)
+            item.Index = index2
+            item.Name = await image.pointerToString(pointer, false)
+        }
+        itemList.push(item)
+    }
+    return itemList
 }

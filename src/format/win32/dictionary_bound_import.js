922011a84ea43f4a93682ac230f3a3aee14d33a4
gaomingyu <874774259@qq.com>
2023-01-18 00:29:27 +0800

    绑定表

--- src/format/win32/dictionary_bound_import.js
+++ src/format/win32/dictionary_bound_import.js
@@ -1,5 +1,21 @@
 export default class {
     static async parse(image, dictionary) {
-        //todo
+        let resultList = []
+        let count = 1
+        for (let index = 0; index < count; index++) {
+            let view = await image.pointerToView(dictionary.VritualAddress + index * 8, 8)
+            let result = {}
+            result.Index = index
+            result.TimeDateStamp = view.getUint32(0, true)
+            result.OffsetModuleName = view.getUint16(4, true)
+            //开头元素定义后续元素数量
+            if (index === 0) {
+                result.NumberOfModuleForwarderRefs = view.getUint16(6, true)
+                count = result.NumberOfModuleForwarderRefs + 1
+            }
+            result.NameString = await image.pointerToString(result.OffsetModuleName, false)
+            resultList.push(result)
+        }
+        return resultList
     }
 }

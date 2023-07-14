af27c6c085ecb7d2c4ef23f4a7653f1c86cd0e4d
gaomingyu <874774259@qq.com>
2023-07-14 11:37:02 +0800

    格式化

--- src/format/pe/dictionary/00export.js
+++ src/format/pe/dictionary/00export.js
@@ -1,29 +1,29 @@
 export async function parse_pe_export(parser, dictionary) {
     const view = await parser.pointerToView(dictionary.VritualAddress)
-    const result = {}
-    result.Charateristics = view.getUint32(0, true)
-    result.TimeDateStamp = view.getUint32(4, true)
-    result.MajorVersion = view.getUint16(8, true)
-    result.MinorVersion = view.getUint16(10, true)
-    result.Name = view.getUint32(12, true)
-    result.Base = view.getUint32(16, true)
-    result.NumberOfFunctions = view.getUint32(20, true)
-    result.NumberOfNames = view.getUint32(24, true)
-    result.AddressOfFunctions = view.getUint32(28, true)
-    result.AddressofNames = view.getUint32(32, true)
-    result.AddressOfNameOrdinals = view.getUint32(36, true)
-    result.NameString = await parser.pointerToString(result.Name, false)
-    result.LIST = await export_list(parser, result)
-    return result
+    const header = {}
+    header.Charateristics = view.getUint32(0, true)
+    header.TimeDateStamp = view.getUint32(4, true)
+    header.MajorVersion = view.getUint16(8, true)
+    header.MinorVersion = view.getUint16(10, true)
+    header.Name = view.getUint32(12, true)
+    header.Base = view.getUint32(16, true)
+    header.NumberOfFunctions = view.getUint32(20, true)
+    header.NumberOfNames = view.getUint32(24, true)
+    header.AddressOfFunctions = view.getUint32(28, true)
+    header.AddressofNames = view.getUint32(32, true)
+    header.AddressOfNameOrdinals = view.getUint32(36, true)
+    header.NameString = await parser.pointerToString(header.Name, false)
+    header.DATA = await export_data(parser, header)
+    return header
 }
 
-async function export_list(parser, result) {
-    const addressView = await parser.pointerToView(result.AddressOfFunctions)
-    const nameView = await parser.pointerToView(result.AddressofNames)
-    const ordinalView = await parser.pointerToView(result.AddressOfNameOrdinals)
+async function export_data(parser, header) {
+    const addressView = await parser.pointerToView(header.AddressOfFunctions)
+    const nameView = await parser.pointerToView(header.AddressofNames)
+    const ordinalView = await parser.pointerToView(header.AddressOfNameOrdinals)
     //读取所有函数名称缓存到map
     const nameMap = new Map()
-    for (let index = 0; index < result.NumberOfNames; index++) {
+    for (let index = 0; index < header.NumberOfNames; index++) {
         const ordinal = ordinalView.getUint16(index * 2, true)
         const name = nameView.getUint32(index * 4, true)
         const nameString = await parser.pointerToString(name, false)
@@ -31,7 +31,7 @@ async function export_list(parser, result) {
     }
     //读取所有地址并关联缓存的名称
     const itemList = []
-    for (let index = 0; index < result.NumberOfFunctions; index++) {
+    for (let index = 0; index < header.NumberOfFunctions; index++) {
         const item = {}
         item.Index = index
         item.Address = addressView.getUint32(index * 4, true)
